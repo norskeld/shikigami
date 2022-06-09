415a632f5237dbd45244ff86156f92f159790bd6
Vladislav Mamon <norskeld@gmail.com>
2022-06-09 11:18:39 +0500

    test: add `useBuilder` tests

---
+++ src/builder.spec.ts
@@ -0,0 +1,24 @@
+import { useBuilder } from './builder'
+
+describe('useBuilder', () => {
+  it('should return a getter and a setter functions', () => {
+    const [getter, setter] = useBuilder()
+
+    expect(getter).toBeInstanceOf(Function)
+    expect(setter).toBeInstanceOf(Function)
+  })
+
+  it('should return a builder with initial values', () => {
+    const [getter] = useBuilder(['one', 'two'])
+    expect(getter()).toBe('onetwo')
+  })
+
+  it('should properly append values', () => {
+    const [getter, setter] = useBuilder()
+
+    setter('one')
+    setter('two')
+
+    expect(getter()).toBe('onetwo')
+  })
+})
