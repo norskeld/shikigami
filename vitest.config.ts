253c65f7bf0513830ae3daccdaaa84405c5744bc
Vladislav Mamon <norskeld@gmail.com>
2022-10-26 00:09:32 +0300

    fix(vitest): fix coverage collection

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
+      all: true,
       provider: 'istanbul',
-      reporter: ['text', 'lcov']
+      reporter: ['text', 'lcov'],
+      include: ['src/**/*.ts']
     }
   }
 })
